0bd8e07b9b2e7b44ffa866060a0885f3cbb0701b
sardor41k42 <sardorjuraqulov38@gmail.com>
2025-06-19 19:19:21 +0500

    Delete search.py

--- search.py
+++
@@ -1,7 +0,0 @@
-
-from aiogram import Router, types
-router = Router()
-
-@router.message(lambda msg: msg.text and not msg.text.startswith('/'))
-async def search_movie(message: types.Message):
-    await message.answer(f"🔍 Qidiruv: '{message.text}' — natija topilmadi.")

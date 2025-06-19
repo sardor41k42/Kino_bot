5ed1ef607bedfebb7084b847d285cbd849b75097
sardor41k42 <sardorjuraqulov38@gmail.com>
2025-06-19 19:19:41 +0500

    Delete start.py

--- start.py
+++
@@ -1,7 +0,0 @@
-
-from aiogram import Router, types
-router = Router()
-
-@router.message(commands=['start'])
-async def cmd_start(message: types.Message):
-    await message.answer("Salom! Kino qidiruv botiga xush kelibsiz.")

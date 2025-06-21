83f32efea418b03b1e1f0c7fd111a24afdb48099
sardor41k42 <sardorjuraqulov38@gmail.com>
2025-06-21 23:29:51 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
+
+kino_baza = {}
+
+def start(update, context):
+    update.message.reply_text("🎬 Salom! Kino nomini yuboring.")
+
+def qidir(update, context):
+    text = update.message.text.lower()
+    javob = kino_baza.get(text, "❌ Bunday kino topilmadi.")
+    update.message.reply_text(javob)
+
+def add(update, context):
+    if update.message.from_user.id != ADMIN_ID:
+        update.message.reply_text("⛔ Siz admin emassiz.")
+        return
+    try:
+        matn = update.message.text.replace("/add ", "")
+        nom, link = matn.split("=")
+        kino_baza[nom.strip().lower()] = link.strip()
+        update.message.reply_text("✅ Qo‘shildi.")
+    except:
+        update.message.reply_text("❗ Format: /add kino=link")
+
+updater = Updater(TOKEN, use_context=True)
+dp = updater.dispatcher
+
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CommandHandler("add", add))
+dp.add_handler(MessageHandler(Filters.text & ~Filters.command, qidir))
+
+updater.start_polling()
+updater.idle()
